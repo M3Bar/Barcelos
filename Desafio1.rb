d0374f3a8f9007fac05762945ae511a76cd87974
M3Bar <152045414+M3Bar@users.noreply.github.com>
2023-11-27 21:04:48 -0400

    Add files via upload
    print "Digite seu nome:"
    nome = gets.chomp
    print "Digite seu sobrenome:"
    sobrenome = gets.chomp
    print "Qual a sua idade?:"
    idade = gets.chomp
    puts "Olá #{nome} #{sobrenome}!!! Seja bem vindo!  Sua idade é #{idade} ."

---
+++ Desafio1.rb
@@ -0,0 +1,8 @@
+print "Digite seu nome:"
+nome = gets.chomp
+print "Digite seu sobrenome:"
+sobrenome = gets.chomp
+print "Qual a sua idade?:"
+idade = gets.chomp
+puts "Olá #{nome} #{sobrenome}!!! Seja bem vindo!  Sua idade é #{idade} ."
+
